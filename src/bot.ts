2acc5a7b9708e8e2d64340a3a7fb00bd45ca094e
Gabriel Timba <gabrieltimba@hotmail.com>
2023-05-09 22:45:43 +0200

    initial commit

---
+++ src/bot.ts
@@ -0,0 +1,70 @@
+import * as qrcode from 'qrcode-terminal';
+import * as dotenv from 'dotenv';
+import * as Whatsapp from 'whatsapp-web.js';
+const { Client, LocalAuth } = Whatsapp;
+
+import { getQuotes } from './quotes'
+dotenv.config();
+
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
+
+client.on('message', async message => {
+	const contact= await message.getContact();
+	const id=message.body.split(':')
+
+	if(message.body === '1') {
+		client.sendMessage(message.from, 'Indique o seu ID no seguinte formato *ID:seu id*\n(Exemplo ID:45adc2)');
+	}
+	else if(message.body === '2') {
+		client.sendMessage(message.from, '*TheDue* é uma plataforma que permite a consulta do extracto de quotas da SPJ de XIBOMANA de forma instantanea');
+	}
+	else if(id[0].toString().toUpperCase()==='ID'){
+		const quote=await getQuotes(id[1])
+
+		if(Object.keys(quote).length !== 0){
+			client.sendMessage(message.from, 
+				'*Extrato de quotas*\n\n'+
+				`Nome:${quote.name}\n`+
+				`Janeiro: ${quote.jan.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN \n`+
+				`Fevereiro:${quote.feb.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN\n`+
+				`Março:${quote.mar.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN\n`+
+				`Abril:${quote.apr.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN\n`+
+				`Maio:${quote.may.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN\n`+
+				`Junho:${quote.june.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN \n`+
+				`Julho:${quote.july.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN \n`+
+				`Agosto:${quote.aug.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN \n`+
+				`Setembro:${quote.sept.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN \n`+
+				`Otubro:${quote.oct.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN\n`+
+				`Novembro:${quote.nov.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN \n`+
+				`Dezembro:${quote.dec.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} MZN\n`
+			);
+		}else{
+			client.sendMessage(message.from, 'ID não encontrado.')
+			client.sendMessage(message.from, 'Indique o seu ID no seguinte formato *ID:seu id*\n(Exemplo ID:45adc2)');
+		}
+
+	}
+	else {
+		client.sendMessage(message.from, 
+			`Olá *${contact.pushname}*,seja bem vindo ao *TheDue*\n\n`+
+			'Selecione uma das opções \n'+
+			'1.Consultar o extrato de quotas da SPJ \n'+
+			'2. Informações da plataforma \n'
+		);
+	}
+});
+
+
+
---
+++ src/quotes.ts
@@ -0,0 +1,71 @@
+import {google} from 'googleapis'
+
+type QuoteType ={
+  name:string,
+  jan:Number,
+  feb:Number,
+  mar:Number,
+  apr:Number,
+  may:Number,
+  june:Number,
+  july:Number,
+  aug:Number,
+  sept:Number,
+  oct:Number,
+  nov:Number,
+  dec:Number
+}
+
+export async function getQuotes(id:string): Promise<QuoteType>{
+  const spreadsheetId=process.env.SpreadsheetId;
+
+  const auth= new google.auth.GoogleAuth({
+    keyFile:"./secrets.json",
+    scopes:"https://www.googleapis.com/auth/spreadsheets"
+  })
+  
+  //Create client instance for auth
+  const client = await auth.getClient();
+  
+  //Instance of google sheets API
+  const googleSheets= google.sheets({ version:'v4'});
+  
+  //Read Rows from spreadSheets
+  const rows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range:"2022!A3:Z"
+  })  
+
+  const rowByID= rows.data.values?.filter((row)=>row[0].toLocaleUpperCase()===id.toLocaleUpperCase())
+
+  if(!rowByID){
+    return <QuoteType>{}
+  }
+  
+  let quote:QuoteType;
+
+  quote=<QuoteType>{}
+  
+  rowByID?.forEach(item=>{
+    quote.name=item[1]?item[1]:''
+    quote.jan=item[2]?Number(item[2]):0
+    quote.feb=item[3]?Number(item[3]):0
+    quote.mar=item[4]?Number(item[4]):0
+    quote.apr=item[5]?Number(item[5]):0
+    quote.may=item[6]?Number(item[6]):0
+    quote.june=item[7]?Number(item[7]):0
+    quote.july=item[8]?Number(item[8]):0
+    quote.aug=item[9]?Number(item[9]):0
+    quote.sept=item[10]?Number(item[10]):0
+    quote.oct=item[11]?Number(item[11]):0
+    quote.nov=item[12]?Number(item[12]):0
+    quote.dec=item[13]?Number(item[13]):0
+  })
+
+  return quote
+
+}
+
+
+
